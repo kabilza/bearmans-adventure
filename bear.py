c10911eceeca81ccf4986fef1d97f28020016a95
kabilza <naruoluvohina@gmail.com>
2019-03-29 13:39:29 +0700

    popia

--- bear.py
+++ bear.py
@@ -1,3 +1,5 @@
+from models import Bear, World
+
 import arcade
 
 SCREEN_WIDTH = 1024
@@ -5,37 +7,30 @@ SCREEN_HEIGHT = 576
 
 SPRITE_SCALING = 0.75
 
-class ModelSprite(arcade.Sprite):
-    def __init__(self, *args, **kwargs):
-        self.model = kwargs.pop('model', None)
-
-        super().__init__(*args, **kwargs)
-
-    def sync_with_model(self):
-        if self.model:
-            self.set_position(self.model.x, self.model.y)
-
-    def draw(self):
-        self.sync_with_model()
-        super().draw()
+class BearSprite:
+    def __init__(self):
+        self.sprite = arcade.Sprite('images/BearSprite1.png',scale = SPRITE_SCALING)
+    def draw(self,x,y):
+        self.sprite.set_position(x,y)
+        self.sprite.draw()
 
 class BearWindow(arcade.Window):
     def __init__(self, width, height):
         super().__init__(width, height)
 
-        self.bear_sprite = ModelSprite('images/BearSprite1.png', scale=SPRITE_SCALING)
+        self.bear_sprite = BearSprite()
         self.bear_sprite.center_x = SCREEN_WIDTH - 90
         self.bear_sprite.center_y = SCREEN_HEIGHT - 500
 
         self.background = arcade.load_texture("images/BG.png")
-
+        self.world = World(width,height)
         arcade.set_background_color(arcade.color.SILVER)
 
     def on_draw(self):
         arcade.start_render()
         arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
 
-        self.bear_sprite.draw()
+        self.bear_sprite.draw(self.world.bear.x,self.world.bear.y)
 
 
 
--- models.py
+++ models.py
@@ -1,13 +1,39 @@
 import arcade.key
 
+MOVEMENT_SPEED = 4
+DIR_STILL = 0
+DIR_UP = 1
+DIR_RIGHT = 2
+DIR_DOWN = 3
+DIR_LEFT = 4
+
+DIR_OFFSETS = {DIR_STILL: (0, 0),
+               DIR_UP: (0, 1),
+               DIR_RIGHT: (1, 0),
+               DIR_DOWN: (0, -1),
+               DIR_LEFT: (-1, 0)}
+
+KEY_MAP = { arcade.key.UP: DIR_UP,
+            arcade.key.DOWN: DIR_DOWN,
+            arcade.key.LEFT: DIR_LEFT,
+            arcade.key.RIGHT: DIR_RIGHT, }
+
 class Bear:
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
+        self.direction = DIR_STILL
 
-    def on_draw(self):
-        self.set_position(self.x, self..y)
+    # def on_draw(self):
+    #     self.set_position(self.x, self..y)
+
+    def move(self, direction):
+        self.x += MOVEMENT_SPEED * DIR_OFFSETS[direction][0]
+        self.y += MOVEMENT_SPEED * DIR_OFFSETS[direction][1]
+
+    def update(self, delta):
+        self.move(self.direction)
 
 class World:
     def __init__(self, width, height):
